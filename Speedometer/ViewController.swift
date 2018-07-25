f3c1e865ebe64ea39d0ebf4e4e076a71bee73352
Benjamin Wierzbanowski <bwierzbo@gmail.com>
2018-07-25 18:21:19 -0400

    Buttons to numbers

--- Speedometer/ViewController.swift
+++ Speedometer/ViewController.swift
@@ -15,9 +15,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var textKNOTS: UILabel!
     @IBOutlet weak var textMIPH: UILabel!
     
+    @IBAction func buttonMPH(_ sender: Any) {
+        textMPH.text = "50"
+    }
     
+    @IBAction func buttonKPH(_ sender: Any) {
+        textKPH.text = "30"
+    }
     
-
+    @IBAction func buttonKNOTS(_ sender: Any) {
+        textKNOTS.text = "3"
+    }
+    
+    @IBAction func buttonMIPH(_ sender: Any) {
+        textMIPH.text = "314"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
